de7c681ac669e4f49087cd759092ab11a058ab11
yujiorama <yujiorama+github@gmail.com>
2014-05-06 23:23:40 +0900

    add blank step definition

---
+++ parking_lot/features/step_definitions/parking_lot_steps.rb
@@ -0,0 +1,9 @@
+
+
+When(/^I park my car in the Valet Parking Lot for (\d+) minutes$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I will have to pay \$ (\d+)\.(\d+)$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
